81601ae0cd0cacfed27ef5a9f37a8b8061b4e3ba
steffen <steffen.weil@proton.me>
2024-11-03 09:09:21 +0100

    init data population

---
+++ data-population/src/main.rs
@@ -0,0 +1,23 @@
+use sqlx::postgres::PgPoolOptions;
+
+// TODO run db migrations
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://pgadmin:pw@localhost/postgres").await?;
+
+    println!("Established connection pool");
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await?;
+
+    assert_eq!(row.0, 150);
+
+    println!("Finished");
+
+    Ok(())
+}
